2409ad2aa2f7dcee00915e6bb679a4344ab7b365
Alexander Heldt <me@alexanderheldt.se>
2023-01-07 18:08:47 +0100

    sombrero: Add `git`

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,10 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [ vim ];
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
