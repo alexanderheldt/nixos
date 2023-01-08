9b8287c9c99935f992e0f471cc12b2beaa803759
Alexander Heldt <me@alexanderheldt.se>
2023-01-08 12:33:19 +0100

    sombrero: cleanup configuration.nix

--- hosts/sombrero/configuration.nix
+++ hosts/sombrero/configuration.nix
@@ -97,14 +97,7 @@
     git
   ];
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-    # Copy the NixOS configuration file and link it from the resulting system
+  # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
