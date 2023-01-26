aa7e99bfc68af84ec4fbf17b14137e38d29b9799
Alexander Heldt <me@alexanderheldt.se>
2023-01-26 15:26:26 +0100

    nix: Pin channel in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,9 @@
 {
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+  };
+
+  outputs = inputs@{ self, nixpkgs }: {
     nixosConfigurations.sombrero = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [ ./hosts/sombrero/configuration.nix ];
