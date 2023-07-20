15f8424685d52908ed686d48ee4cf0fa885c7cbd
Alexander Heldt <me@alexanderheldt.se>
2023-07-20 19:26:40 +0200

    bennu: Add nixos-hardware module

--- flake.nix
+++ flake.nix
@@ -4,15 +4,17 @@
   inputs = {
     nixpkgs2305.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs2211.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs2305";
   };
   
-  outputs = { self, nixpkgs2305, nixpkgs2211, home-manager, ... }: {
+  outputs = { self, nixpkgs2305, nixpkgs2211, nixos-hardware, home-manager, ... }: {
     nixosConfigurations = {
       bennu = nixpkgs2305.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ 
+          nixos-hardware.nixosModules.lenovo-thinkpad-x1-10th-gen
           ./hosts/bennu/configuration.nix
           home-manager.nixosModules.home-manager
           {
