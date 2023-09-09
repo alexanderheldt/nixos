641c333520ba43aea5dcd70a3e5e2ce43965f7b7
Alexander Heldt <me@alexanderheldt.se>
2023-09-09 19:13:18 +0200

    pinwheel: Remove duplicated `age.identityPaths`

--- hosts/pinwheel/modules/ssh/default.nix
+++ hosts/pinwheel/modules/ssh/default.nix
@@ -17,37 +17,30 @@
     };
   };
 
-  age = {
-    identityPaths = [
-      "/etc/ssh/pinwheel"
-      "/home/alex/.ssh/alex.pinwheel"
-    ];
-
-    secrets = {
-      "alex.pinwheel-sombrero" = {
-        file = ../../../../secrets/pinwheel/alex.pinwheel-sombrero.age;
-        path = "/home/alex/.ssh/alex.pinwheel-sombrero";
-        owner = "alex";
-        group = "users";
-      };
-      "alex.pinwheel-sombrero.pub" = {
-        file = ../../../../secrets/pinwheel/alex.pinwheel-sombrero.pub.age;
-        path = "/home/alex/.ssh/alex.pinwheel-sombrero.pub";
-        owner = "alex";
-        group = "users";
-      };
-      "alex.pinwheel-github.com" = {
-         file = ../../../../secrets/pinwheel/alex.pinwheel-github.com.age;
-         path = "/home/alex/.ssh/alex.pinwheel-github.com";
-         owner = "alex";
-         group = "users";
-      };
-      "alex.pinwheel-github.com.pub" = {
-         file = ../../../../secrets/pinwheel/alex.pinwheel-github.com.pub.age;
-         path = "/home/alex/.ssh/alex.pinwheel-github.com.pub";
-         owner = "alex";
-         group = "users";
-      };
+  age.secrets = {
+    "alex.pinwheel-sombrero" = {
+      file = ../../../../secrets/pinwheel/alex.pinwheel-sombrero.age;
+      path = "/home/alex/.ssh/alex.pinwheel-sombrero";
+      owner = "alex";
+      group = "users";
+    };
+    "alex.pinwheel-sombrero.pub" = {
+      file = ../../../../secrets/pinwheel/alex.pinwheel-sombrero.pub.age;
+      path = "/home/alex/.ssh/alex.pinwheel-sombrero.pub";
+      owner = "alex";
+      group = "users";
+    };
+    "alex.pinwheel-github.com" = {
+       file = ../../../../secrets/pinwheel/alex.pinwheel-github.com.age;
+       path = "/home/alex/.ssh/alex.pinwheel-github.com";
+       owner = "alex";
+       group = "users";
+    };
+    "alex.pinwheel-github.com.pub" = {
+       file = ../../../../secrets/pinwheel/alex.pinwheel-github.com.pub.age;
+       path = "/home/alex/.ssh/alex.pinwheel-github.com.pub";
+       owner = "alex";
+       group = "users";
     };
   };
 }
