a95d70734eda4ef928723265f1912d2a5a52dc61
Alexander Heldt <me@alexanderheldt.se>
2023-09-24 15:24:26 +0200

    pinwheel: Add keybinding for muting the microphone

--- hosts/pinwheel/modules/sound/default.nix
+++ hosts/pinwheel/modules/sound/default.nix
@@ -23,6 +23,7 @@
           ", XF86AudioRaiseVolume, exec, ${pkgs.wireplumber}/bin/wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 2%+"
           ", XF86AudioLowerVolume, exec, ${pkgs.wireplumber}/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-"
           ", XF86AudioMute, exec, ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioMicMute, exec, ${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
         ];
       };
     };
