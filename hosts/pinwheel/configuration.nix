6d1053c891bfc0c4bc29bd236ffe94af9e966eaa
Alexander Heldt <me@alexanderheldt.se>
2023-10-03 09:40:32 +0200

    pinwheel: Increase `fs.inotify.max_user_instances` to `1024`

--- hosts/pinwheel/configuration.nix
+++ hosts/pinwheel/configuration.nix
@@ -47,6 +47,12 @@
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
 
+    kernel = {
+      sysctl = {
+        "fs.inotify.max_user_instances" = 1024; # default: 128
+      };
+    };
+
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
