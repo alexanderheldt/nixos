db23cf2ca71955302bee005899eb5ddbc6d4bf4a
Alexander Heldt <me@alexanderheldt.se>
2023-10-17 23:02:38 +0200

    pinwheel: Style `bemenu` to match `waybar`

--- hosts/pinwheel/modules/bemenu/default.nix
+++ hosts/pinwheel/modules/bemenu/default.nix
@@ -1,6 +1,27 @@
 { pkgs, lib, config, ... }:
 let
   hyprlandEnabled = config.mod.hyprland.enable;
+
+  bmr =
+    let
+      foreground = "#f9c22b";
+      foreground-dim = "#a57b06";
+      background = "#262626";
+    in
+      pkgs.writeShellScript "bmr" ''
+      ${pkgs.bemenu}/bin/bemenu-run \
+        --fn 'DejaVuSansM Nerd Font Mono 16' \
+        --hp 10 \
+        --line-height 38 \
+        --cw 12 \
+        --ch 25 \
+        --fixed-height \
+        --ff "${foreground}" --fb "${background}" \
+        --hf "${foreground}" --hb "${background}" \
+        --af "${foreground-dim}" --ab "${background}" \
+        --nf "${foreground-dim}" --nb "${background}" \
+        --prompt ""
+  '';
 in
 {
   home-manager.users.alex = {
@@ -8,9 +29,7 @@ in
 
     wayland.windowManager.hyprland = lib.mkIf hyprlandEnabled {
       settings = {
-        bind = [
-          "$mod, SPACE, exec, ${pkgs.bemenu}/bin/bemenu-run --fn 'DejaVuSansM Nerd Font Mono 14'"
-        ];
+        bind = [ "$mod, SPACE, exec, ${bmr}" ];
       };
     };
   };
