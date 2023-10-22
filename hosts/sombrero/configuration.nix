bc4e205a62dcf252e4164938f5f1f29713e60050
Alexander Heldt <me@alexanderheldt.se>
2023-10-22 20:29:03 +0200

    sombrero: Cleanup `configuration.nix` whitespace

--- hosts/sombrero/configuration.nix
+++ hosts/sombrero/configuration.nix
@@ -168,7 +168,6 @@
     system = "aarch64-linux";
   };
 
-
   mod = {
     docker.enable = true;
     nginx.enable = true;
@@ -189,5 +188,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
-
 }
