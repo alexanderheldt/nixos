9970f9db14858475e9dde6f151b2e8fa87818346
Alexander Heldt <me@alexanderheldt.se>
2023-10-22 17:35:00 +0200

    sombrero: Add `docker` module

---
+++ hosts/sombrero/modules/docker/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, config, ... }:
+let
+  enabled = config.mod.docker.enable;
+in
+{
+  options = {
+    mod.docker = {
+      enable = lib.mkEnableOption "enable docker module";
+    };
+  };
+
+  config = lib.mkIf enabled {
+    virtualisation = {
+      docker = {
+        enable = true;
+      };
+
+      oci-containers = {
+        backend = "docker";
+      };
+    };
+
+    users.users.alex.extraGroups = [ "docker" ];
+
+    home-manager.users.alex = {
+      home.packages = [ pkgs.docker-compose ];
+    };
+  };
+}
