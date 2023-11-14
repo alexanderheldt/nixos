65ba8d7b94af9fb9289555b7f1fa3b0515357bee
Alexander Heldt <me@alexanderheldt.se>
2023-11-14 17:10:36 +0100

    pinwheel: Add `localisation` module

--- hosts/pinwheel/configuration.nix
+++ hosts/pinwheel/configuration.nix
@@ -20,24 +20,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Stockholm";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sv_SE.UTF-8";
-    LC_IDENTIFICATION = "sv_SE.UTF-8";
-    LC_MEASUREMENT = "sv_SE.UTF-8";
-    LC_MONETARY = "sv_SE.UTF-8";
-    LC_NAME = "sv_SE.UTF-8";
-    LC_NUMERIC = "sv_SE.UTF-8";
-    LC_PAPER = "sv_SE.UTF-8";
-    LC_TELEPHONE = "sv_SE.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.alex = {
     isNormalUser = true;
